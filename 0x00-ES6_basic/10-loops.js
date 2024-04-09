3aeab53df97540e6d740922d5cff6103880abefb
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-09 15:23:02 +0000

    For of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [i, val] of array.entries()) {
+  for (const i of array.keys()) {
+    const val = array[i];
     array[i] = appendString + val;
   }
 
