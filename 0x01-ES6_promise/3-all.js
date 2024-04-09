e48aa4a74c39dbef4ff2ac9555910e6f75334117
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-09 18:20:13 +0000

    Resolve all promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()]).then((values) => {
+  return Promise.allSettled([uploadPhoto(), createUser()]).then((values) => {
     console.log(`${values[0].value.body} ${values[1].value.firstName} ${values[1].value.lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
