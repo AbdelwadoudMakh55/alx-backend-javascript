186ac333765ac7eb90c98577a376850e42c5c731
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-09 17:01:50 +0000

    Then, catch, finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    const o = {
-      status: 200,
-      body: 'Success',
-    };
-    return o;
-  }).catch(() => {
-    const err = new Error();
-    return err;
-  }).finally(() => {
+  promise.then(() => ({
+    status: 200,
+    body: 'Success',
+  })).catch(() => new Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }
