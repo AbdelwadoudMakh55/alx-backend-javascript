c8d75731f9503c8a324c04001254da4dae9005b7
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-11 13:18:33 +0000

    Creating objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,6 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const c1 = new ClassRoom(19);
   const c2 = new ClassRoom(20);
-  const c3 = new ClassRoom(24);
+  const c3 = new ClassRoom(34);
   return [c1, c2, c3];
 }
