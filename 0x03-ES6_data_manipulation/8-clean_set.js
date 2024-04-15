5bc2e585d00a7418824bf80677640de41d923c4d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-15 18:21:05 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === ' '.repeat(startString.length)) {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   let result = '';
   set.forEach((word) => {
-    if (word.startsWith(startString)) {
+    if (typeof word === 'string' && word.startsWith(startString)) {
       result += `${word.slice(startString.length)}-`;
     }
   });
