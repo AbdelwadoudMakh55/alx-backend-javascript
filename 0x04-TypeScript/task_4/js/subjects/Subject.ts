555f2be38527818940a22081989f3726aac21495
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-20 15:35:09 +0000

    class instances

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,10 +2,6 @@ namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
-    constructor(teacher: Teacher) {
-      this.teacher = teacher;
-    }
-    
     setTeacher(teacher: Teacher) {
     	this.teacher = teacher;
     }
