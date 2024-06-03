6dd3fca80287a074ecfc2d26c3ec6643d3a5c69a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-03 16:21:36 +0100

    Close then ouptut message to STDOUT

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString().trim()}`);
+  process.exit();
 });
-
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
+process.on('exit', () => {
   console.log('This important software is now closing');
 });
