ca5e56dbc89b4e77d3322a0790501d43d133c2b1
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-03 19:05:21 +0100

    Parse .csv file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
 
-function countStudents (path) {
-  fs.readFile(path, "utf-8", (err, data) => {
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
-    lines = data.split("\n");
+    const lines = data.split('\n');
     let numberOfStudents = -1;
-    let fields = {};
-    lines.forEach(element => {
-      columns = element.split(",");
-      if (element !== "") {
-        numberOfStudents++;
+    const fields = {};
+    lines.forEach((element) => {
+      const columns = element.split(',');
+      if (element !== '') {
+        numberOfStudents += 1;
       }
       if (columns[3] !== 'field' && columns[3] in fields) {
-        fields[columns[3]][0]++;
+        fields[columns[3]][0] += 1;
         fields[columns[3]].push(columns[0]);
       } else if (columns[3] !== 'field') {
         fields[columns[3]] = [1];
@@ -23,8 +23,8 @@ function countStudents (path) {
     });
     console.log(`Number of students: ${numberOfStudents}`);
     for (const [key, value] of Object.entries(fields)) {
-      ListOfStudents = value.slice(1).join(", ");
-      out = `Number of students in ${key}: ${value[0]}. List: ${ListOfStudents}`;
+      const ListOfStudents = value.slice(1).join(', ');
+      const out = `Number of students in ${key}: ${value[0]}. List: ${ListOfStudents}`;
       console.log(out);
     }
   });
