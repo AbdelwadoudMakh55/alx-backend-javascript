a8e3bb41e4ca6dcac8af23b3e312f2f7093ebf1f
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-03 18:59:04 +0100

    Close then ouptut message to STDOUT

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  process.exit();
+  process.exit(0);
 });
 process.on('exit', () => {
   console.log('This important software is now closing');
