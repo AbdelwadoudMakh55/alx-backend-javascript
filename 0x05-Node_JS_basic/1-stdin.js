6d84ad0f6f6d5842a9126578f5cee9439c64d798
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-04 23:16:50 +0100

    Handle case of TTY and pipe

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,18 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString().trim()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString().trim()}`);
-    process.exit();
-  });
-}
-process.on('exit', () => {
-  console.log('This important software is now closing');
+
+let input = '';
+process.stdin.on('data', (data) => {
+  input = data;
+  if (process.stdin.isTTY) {
+    console.log(`Your name is: ${input.trim()}`);
+    process.stdin.pause();
+  }
+});
+
+process.stdin.on('end', () => {
+  if (!process.stdin.isTTY) {
+    console.log(`Your name is: ${input.trim()}`);
+    console.log('This important software is now closing');
+  }
 });
