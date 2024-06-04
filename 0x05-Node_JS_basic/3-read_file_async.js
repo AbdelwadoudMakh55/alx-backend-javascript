11b0234ab08d5993785028814ccfd180af85ff75
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-05 00:38:57 +0100

    Fix csv parsing

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,32 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8')
-      .then((data) => {
-        const lines = data.split('\n');
-        let numberOfStudents = -1;
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line !== '');
         const fields = {};
-        lines.forEach((element) => {
+        lines.slice(1).forEach((element) => {
           const columns = element.split(',');
-          if (element !== '') {
-            numberOfStudents += 1;
-          }
-          if (columns[3] !== 'field' && columns[3] in fields) {
+          if (columns[3] in fields) {
             fields[columns[3]][0] += 1;
             fields[columns[3]].push(columns[0]);
-          } else if (columns[3] !== 'field') {
+          } else {
             fields[columns[3]] = [1];
+            fields[columns[3]].push(columns[0]);
           }
         });
-        console.log(`Number of students: ${numberOfStudents}`);
+        console.log(`Number of students: ${lines.length - 1}`);
         for (const [key, value] of Object.entries(fields)) {
           const ListOfStudents = value.slice(1).join(', ');
           const out = `Number of students in ${key}: ${value[0]}. List: ${ListOfStudents}`;
           console.log(out);
         }
         resolve();
-      })
-      .catch(() => {
-        reject(new Error('Cannot load the database'));
-      });
+      }
+    });
   });
 }
 module.exports = countStudents;
