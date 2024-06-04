02a359bc6e547ca1d1ad6473de61af99f95385ca
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-04 13:22:50 +0100

    Set encoding to utf-8

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  process.exit(0);
-});
-process.on('exit', () => {
   console.log('This important software is now closing');
+  process.exit();
 });
