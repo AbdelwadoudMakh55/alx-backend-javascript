4efa0917b61050102b11f3d874be7d6cd8185812
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-04 23:27:14 +0100

    Fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-  console.log('Your name is: ${data.toString().trim()}');
+  console.log(`Your name is: ${data.toString().trim()}`);
   process.exit();
 });
 
