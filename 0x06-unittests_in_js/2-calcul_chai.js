30cb288e80c24f370592c226b5f70a2f13adbe58
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-05 12:41:28 +0100

    Use chai instead of assert

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,14 @@
+function calculateNumber(type, a, b) {
+  if (type === 'SUM')
+    return Math.round(a) + Math.round(b);
+  else if (type === 'SUBSTRACT')
+    return Math.round(a) - Math.round(b);
+  else if (type == 'DIVIDE') {
+    round_b = Math.round(b);
+    if (round_b === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / round_b;
+  }
+}
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,36 @@
+const calculateNumber = require('./2-calcul_chai')
+const expect = require('chai').expect;
+
+describe('SUM', () => {
+  it('Add 2 numbers', () => {
+    expect(calculateNumber('SUM', 4.1, -5.2)).to.equal(-1);
+  });
+  it('Add 2 numbers', () => {
+    expect(calculateNumber('SUM', 7.6, -5)).to.equal(3);  
+  });
+  it('Add 2 numbers', () => {
+    expect(calculateNumber('SUM', 4.2, 5.2)).to.equal(9);
+  });
+});
+describe('SUBSTRACT', () => {
+  it('Substract b from a', () => {
+    expect(calculateNumber('SUBSTRACT', 4.1, -5.2)).to.equal(9);
+  });
+  it('Substract b from a', () => {
+    expect(calculateNumber('SUBSTRACT', 7.6, -5)).to.equal(13);  
+  });
+  it('Substract b from a', () => {
+    expect(calculateNumber('SUBSTRACT', 10.5, 7.3)).to.equal(4);
+  });
+});
+describe('DIVIDE', () => {
+  it('Divide a by b', () => {
+    expect(calculateNumber('DIVIDE', 4.1, 0.1)).to.equal('Error');
+  });
+  it('Divide a by b', () => {
+    expect(calculateNumber('DIVIDE', 7.6, 3.6)).to.equal(2);  
+  });
+  it('Divide a by b', () => {
+    expect(calculateNumber('DIVIDE', 10.4, 4.6)).to.equal(2);
+  });
+});
