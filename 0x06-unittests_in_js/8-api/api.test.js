10d92af6d0e2bc2200b3deefc55eca019c9e9559
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-06 19:53:59 +0100

    Integration test for the API

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,20 @@
 const { expect } = require('chai');
 const request = require('request');
 
-describe('Index page', () => {
+describe('Index page test', () => {
   it('API test', () => {
     request('localhost:7865', (err, data) => {
       if (err) {
         return;
       }
       expect(data.statusCode).to.equal(200);
+    });
+  });
+  it('API test 1', () => {
+    request('localhost:7865', (err, data) => {
+      if (err) {
+        return;
+      }
       expect(data).to.equal('Welcome to the payment system');
     });
   });
